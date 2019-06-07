c8f17b63340d50b6c257b3523f20d521581047df
majesty86 <tladislas@gmail.com>
2019-06-07 13:08:37 -0400

    Updates

---
+++ precache-manifest.3e68927e86b94c858e4e256f6820f84c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364255e7aef439b46c09f4e2c816f080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b5f0a4de996789324e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e8b5f0a4de996789324e",
+    "url": "/static/js/main.ccc4fd9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
